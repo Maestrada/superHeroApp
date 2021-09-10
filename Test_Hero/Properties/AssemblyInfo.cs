3577ebae46f92c8a705282e5f1cfdb530cef26df
maestrada <javiestrada90@gmail.com>
2021-09-10 14:07:08 -0600

    Añadiendo proyecto test, que antes no cargaba.

---
+++ Test_Hero/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test_Hero")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test_Hero")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbae75c6-ac96-43af-8b6e-5511d86c9f7d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test_Hero/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestHero
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async void TestSearchHero()
+        {
+            superHeroApp.Models.RequestAccess.RequestAccess method = new superHeroApp.Models.RequestAccess.RequestAccess();
+            string result = await method.SearchHero("");
+            result.Contains("id");
+        }
+
+        [TestMethod]
+        public async void TestGetHeroValidHero()
+        {
+            superHeroApp.Models.RequestAccess.RequestAccess method = new superHeroApp.Models.RequestAccess.RequestAccess();
+            string result = await method.GetHero(2);
+            Assert.IsTrue(!result.Contains("error"));
+        }
+
+
+        [TestMethod]
+        public async void TestGetHeroInvalidHero()
+        {
+            superHeroApp.Models.RequestAccess.RequestAccess method = new superHeroApp.Models.RequestAccess.RequestAccess();
+            string result = await method.GetHero(0);
+            Assert.IsTrue(result.Contains("error"));
+        }
+    }
+}
